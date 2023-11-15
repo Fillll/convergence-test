b8e09e501b3b9ea46c467d1febe2474ec28f6208
Fil <git@alexfil.com>
2023-11-14 23:11:32 -0500

    Continue the test functionallity.

--- convergence.py
+++ convergence.py
@@ -92,32 +92,55 @@ def generate_an_image_from_prompt(openai_api_key, image_generation_prompt, filen
 
 
 def main(apikey_filename, generation_prompt, folder_name, description_prompt, number_of_iteration):
-    """
-    Main function to orchestrate the image generation and description process.
-
-    Args:
-    apikey_filename (str): Path to the file containing OpenAI API key.
-    generation_prompt (str): Initial prompt for generating images.
-    folder_name (str): Folder to store generated images.
-    description_prompt (str): Prompt for describing images.
-    number_of_iteration (int): Number of iterations to perform.
-    """
+    # Open and read the API key from the specified file.
     with open(apikey_filename, "r") as file:
         openai_api_key = file.read().strip()
 
-    for iteration in range(number_of_iteration):
-        start_time = time.time()
-        new_image_filename = f"{folder_name}/{str(iteration).zfill(3)}.jpeg"
-        generate_an_image_from_prompt(openai_api_key, generation_prompt, new_image_filename)
+    # Checking for existing files in the specified folder.
+    # The list comprehension iterates over all items in the folder,
+    # and includes only those that are files (ignoring directories).
+    existing_files = [f for f in os.listdir(folder_name) if os.path.isfile(os.path.join(folder_name, f))]
 
-        description = describe_image(new_image_filename, openai_api_key, description_prompt)
+    # If there are existing files, determine the highest numbered file.
+    # This is done by splitting each filename at the dot (assuming the format is 'number.extension'),
+    # converting the number part to an integer, and finding the maximum value.
+    if existing_files:
+        highest_number = max([int(f.split('.')[0]) for f in existing_files])
+        starting_iteration = highest_number + 1
+    else:
+        # If there are no existing files, start from the beginning.
+        starting_iteration = 0
+
+    # Special case handling: if starting from scratch (i.e., no existing images),
+    # generate the first image based on the initial prompt.
+    if starting_iteration == 0:
+        generate_an_image_from_prompt(openai_api_key, generation_prompt, f"{folder_name}/000.jpeg")
+        starting_iteration = 1
+
+    # Main loop for generating and describing images.
+    # This loop starts from either the first iteration (if new) or the next iteration (if continuing).
+    for iteration in range(starting_iteration, starting_iteration + number_of_iteration):
+        start_time = time.time()  # Record the start time of the iteration.
+
+        # Filenames for the previous and next image.
+        # 'zfill(3)' ensures the iteration number is zero-padded to 3 digits.
+        prev_image_filename = f"{folder_name}/{str(iteration - 1).zfill(3)}.jpeg"
+        next_image_filename = f"{folder_name}/{str(iteration).zfill(3)}.jpeg"
+
+        # Describe the previous image to generate a prompt for the next image.
+        description = describe_image(prev_image_filename, openai_api_key, description_prompt)
+
+        # Generate the next image based on the description of the previous one.
+        generate_an_image_from_prompt(openai_api_key, description, next_image_filename)
+
+        # Calculate the time spent on this iteration.
         spent_time = time.time() - start_time
-        print("\n\n")
+        print("\n")
         print("-" * 33)
-        print(f"Iteration: {str(iteration).zfill(3)}. Time: {spent_time:.3f} s.")
+        print(f" Iteration: {str(iteration).zfill(3)}. Time: {spent_time:.3f} s.")
         print("-" * 33)
         print(description)
-        generation_prompt = description
+        print("\n")
 
 
 if __name__ == "__main__":
